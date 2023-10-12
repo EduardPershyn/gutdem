547dff1d14c7f3d7fad99f17cd84d408223d6739
Eduard Pershyn <eduard.pershyn@zenseact.com>
2023-10-13 01:06:22 +0300

    demnft echidna

--- contracts/DemNft/test/echidna/DemNft.echidna.sol
+++ contracts/DemNft/test/echidna/DemNft.echidna.sol
@@ -7,7 +7,7 @@ import {LibDemNft} from "../../libraries/LibDemNft.sol";
 
 contract DemNftEchidna is DemNft  {
 
-    //event Debug(address who, uint256 amount);
+    event Debug(address from, address to, uint256 id);
 
     address constant user1 = address(0x10000);
     address constant user2 = address(0x20000);
@@ -27,11 +27,25 @@ contract DemNftEchidna is DemNft  {
         LibDemNft.mint(maxNft / 2, user2);
     }
 
-    function checkTotalSupply() public {
+    function transferAndCheck(uint256 value) public {
+        uint256 index = value % (this.balanceOf(msg.sender) + 1);
+        uint256 tokenId = this.tokenOfOwnerByIndex(msg.sender, index);
+        if (msg.sender != user1) {
+            this.transferFrom(msg.sender, user1, tokenId);
+            emit Debug(msg.sender, user1, tokenId);
+        } else {
+            this.transferFrom(msg.sender, user2, tokenId);
+            emit Debug(msg.sender, user2, tokenId);
+        }
+        checkTotalSupply();
+        checkBalances();
+    }
+
+    function checkTotalSupply() internal {
         assert(this.totalSupply() <= maxNft);
     }
 
-    function checkBalances() public {
+    function checkBalances() internal {
         assert(this.balanceOf(user1) <= maxNft);
         assert(this.balanceOf(user2) <= maxNft);
     }
